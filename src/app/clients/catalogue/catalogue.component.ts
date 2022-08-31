c688e77a514af9db8349aa82ce2ca5c1822713b2
Galo Ainstein <galoainstein@gmail.com>
2022-08-30 21:22:41 -0300

    adjusts to match main

--- src/app/clients/catalogue/catalogue.component.ts
+++ src/app/clients/catalogue/catalogue.component.ts
@@ -7,38 +7,47 @@ import { LoginService } from 'src/app/shared/services/login.service';
 @Component({
   selector: 'app-catalogue',
   templateUrl: './catalogue.component.html',
-  styleUrls: ['./catalogue.component.scss'],
+  styleUrls: ['./catalogue.component.scss']
 })
 export class CatalogueComponent implements OnInit {
+
   private password: string;
   private downloadButton: HTMLElement;
+  private downloadUrban: HTMLElement;
 
-  private fullCatalogueURL: string =
-    '../../../assets/catalogues/CATALOGUE MK.pdf';
+  private fullCatalogueURL: string = '../../../catalogues/CATALOGUE MK.pdf'
+
+  public viewButtonText: string;
 
   constructor(
     private loginService: LoginService,
     private route: ActivatedRoute,
     private translate: TranslateService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.setPassword();
+    this.setPassword()
 
-    if (!this.loginService.validatePassword(this.password)) {
+    if (!this.loginService.validatePassword(this.password)){
       window.location.pathname = '/buyers/login';
     }
 
-    this.downloadButton = document.querySelector(
-      '#download-catalogue'
-    ) as HTMLElement;
+    this.viewButtonText = this.translate.instant('buyers.catalogue.view')
+
+    this.downloadButton = document.querySelector('#download-catalogue') as HTMLElement;
 
     this.downloadButton.addEventListener('click', () => {
-      window.location.pathname = `/buyers/login/view/${this.password}.full`;
-    });
+      window.location.pathname = `/buyers/login/view/${this.password}.full`
+    })
+    
+    this.downloadUrban = document.querySelector('#download-catalogue-urban') as HTMLElement;
+    this.downloadUrban.addEventListener('click', () => {
+      window.location.pathname = `/buyers/login/view/${this.password}.urban`
+    })
+    
   }
 
-  private setPassword() {
+  private setPassword(){
     const param = this.route.snapshot.paramMap.get('password');
     if (param) {
       this.password = param;
@@ -47,7 +56,4 @@ export class CatalogueComponent implements OnInit {
     }
   }
 
-  private openPDF(url: string) {
-    window.open(url, '_blank');
-  }
 }
--- src/app/clients/catalogue/visualize-pdf/visualize-pdf.component.ts
+++ src/app/clients/catalogue/visualize-pdf/visualize-pdf.component.ts
@@ -6,34 +6,40 @@ import { LoginService } from 'src/app/shared/services/login.service';
 @Component({
   selector: 'app-visualize-pdf',
   templateUrl: './visualize-pdf.component.html',
-  styleUrls: ['./visualize-pdf.component.scss'],
+  styleUrls: ['./visualize-pdf.component.scss']
 })
 export class VisualizePdfComponent implements OnInit {
+
   private password: string;
-  private file: string;
+  public file: string;
+  private iframe: HTMLIFrameElement;
 
   constructor(
     private loginService: LoginService,
     private route: ActivatedRoute
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.setParams();
+    this.setParams()
 
-    if (!this.loginService.validatePassword(this.password)) {
-      window.location.pathname = '/buyers/login';
+    if (!this.loginService.validatePassword(this.password)){
+      window.location.pathname = '/clients/login';
     }
   }
 
-  private setParams() {
-    const param: string | null = this.route.snapshot.paramMap.get('file');
+  private setParams(){
+    const param: string|null = this.route.snapshot.paramMap.get('file');
     if (param) {
-      const paramArray: string[] = param.split('.');
+      const paramArray: string[] = param.split('.')
       this.password = paramArray[0];
       this.file = paramArray[1];
+
+      this.iframe = document.querySelector(`#${this.file}`) as HTMLIFrameElement;
+      this.iframe.hidden = false;
     } else {
       this.password = '';
       this.file = '';
     }
   }
+
 }
